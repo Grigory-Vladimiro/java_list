396ab9d518e07e5b5e8ec05029d2b70115eb8c13
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-02 21:58:09 +0100

    method1_test_coverage_increase80%+main_test

--- java_list/src/test/java/dev/grigory/java_list/DaysOfWeekManagerTest.java
+++ java_list/src/test/java/dev/grigory/java_list/DaysOfWeekManagerTest.java
@@ -1,5 +1,7 @@
 package dev.grigory.java_list;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
@@ -10,5 +12,14 @@ public class DaysOfWeekManagerTest {
         DaysOfWeekManager manager = new DaysOfWeekManager();
         manager.createDaysList();
         Assertions.assertEquals(List.of("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"), manager.getDays());
+    }
+    @Test
+    void testMain() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+        DaysOfWeekManager.main(new String[]{});
+        System.setOut(originalOut);
+        Assertions.assertFalse(outputStream.toString().trim().isEmpty());
     }
 }
