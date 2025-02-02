86c10f78456fd80883210551518860881ce702ca
Grigory-Vladimiro <grigori.vladimiro@gmail.com>
2025-02-02 21:33:07 +0100

    method1_test_coverage47%

--- java_list/src/test/java/dev/grigory/java_list/DaysOfWeekManagerTest.java
+++ java_list/src/test/java/dev/grigory/java_list/DaysOfWeekManagerTest.java
@@ -1,8 +1,14 @@
 package dev.grigory.java_list;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class DaysOfWeekManagerTest {
-    
+    @Test
+    void testGetDays() {
+        DaysOfWeekManager manager = new DaysOfWeekManager();
+        manager.createDaysList();
+        Assertions.assertEquals(List.of("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"), manager.getDays());
+    }
 }
